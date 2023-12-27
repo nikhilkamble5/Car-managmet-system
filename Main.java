fe3feffd52aae2a04e5423a1fcd461218fe71474
nikhilkamble5 <153905674+nikhilkamble5@users.noreply.github.com>
2023-12-27 19:43:51 +0530

    first commit

---
+++ Main.java
@@ -0,0 +1,16 @@
+
+/**
+ * InnerMain
+ */
+public interface utility {
+    public void get_details();
+    public void set_details();
+
+    
+}
+public class Main {
+    
+    static void main_menu(){
+        
+    }
+}
